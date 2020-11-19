8ae0d1884e3e451e9f83454ae60d6095103b894f
Waleed Samy <waleedsamygd@protonmail.com>
2020-11-19 10:34:51 +0100

    Rc<T> that holds a RefCell<T>, you can get a value that can have multiple owners and that you can mutate

--- src/bin/learn_box_rc.rs
+++ src/bin/learn_box_rc.rs
@@ -1,8 +1,15 @@
+use std::cell::RefCell;
 use std::rc::Rc;
 #[derive(Debug)]
 enum List<T> {
-    Cons(T, Rc<List<T>>),
+    Cons(Rc<RefCell<T>>, Rc<List<T>>),
     Nil,
+}
+
+impl<T> List<T> {
+    fn create(elem: T) -> Rc<RefCell<T>> {
+        Rc::new(RefCell::new(elem))
+    }
 }
 
 fn main() {
@@ -11,7 +18,9 @@ fn main() {
     let b = Box::new(10);
     println!("b= {} ({:p})", b, &b);
 
-    let list_a: Rc<List<i32>> = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let value = List::create(5); // owned by list_a too
+
+    let list_a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
     println!("list_a= {:?} ({:p})", list_a, &list_a);
     println!("count of owner of list_a {}", Rc::strong_count(&list_a));
 
@@ -26,12 +35,18 @@ fn main() {
     assert_eq!(5, x);
     assert_eq!(5, *y);
 
-    let _list_b = Cons(3, Rc::clone(&list_a));
+    let list_b = Cons(List::create(3), Rc::clone(&list_a));
     println!("count of owner of list_a {}", Rc::strong_count(&list_a));
     {
-        let _list_c = Cons(4, Rc::clone(&list_a));
+        let _list_c = Cons(List::create(4), Rc::clone(&list_a));
         println!("count of owner of list_a {}", Rc::strong_count(&list_a));
     }
-    let _list_d = Cons(4, Rc::clone(&list_a));
+    let list_d = Cons(List::create(4), Rc::clone(&list_a));
     println!("count of owner of list_a {}", Rc::strong_count(&list_a));
+
+    *value.borrow_mut() += 10;
+
+    println!("list_a= {:?} ({:p})", list_a, &list_a);
+    println!("list_b= {:?} ({:p})", list_b, &list_b);
+    println!("list_d= {:?} ({:p})", list_d, &list_d);
 }
