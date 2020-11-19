4617e7948a789623a98ddb3a51522315d26972fc
Waleed Samy <waleedsamygd@protonmail.com>
2020-11-19 16:39:52 +0100

    ch16-02-message-passing.html

---
+++ src/bin/learn_channels.rs
@@ -0,0 +1,29 @@
+use std::sync::mpsc;
+use std::{thread, time};
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let val = String::from("Hello");
+        tx.send(val).unwrap(); // send take ownership of val
+    });
+
+    // will block the main thread’s execution and
+    // wait until a value is sent down the channel.
+    // `try_recv` doesn’t block
+    let received = rx.recv().unwrap();
+    println!("Got {}!", received);
+
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        for i in 1..10 {
+            let val = String::from(format!("Hello {}", i));
+            tx.send(val).unwrap(); // send take ownership of val
+            thread::sleep(time::Duration::from_secs(1))
+        }
+    });
+
+    for v in rx {
+        println!("Got {}!", v);
+    }
+}
