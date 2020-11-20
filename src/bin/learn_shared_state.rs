343d4059c33a60d635a97d7ab5d45f32d200d31f
Waleed Samy <waleedsamygd@protonmail.com>
2020-11-20 13:30:39 +0100

    use sync::Arc like rc::Rc but thread safe

--- src/bin/learn_shared_state.rs
+++ src/bin/learn_shared_state.rs
@@ -1,13 +1,27 @@
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 
 fn main() {
     let mx = Mutex::new(5);
-
     {
         let mut m = mx.lock().unwrap();
         *m += 1;
         // lock released here (Drop)
     }
+    println!("{:?}", mx);
 
-    println!("{}", *(mx.lock().unwrap()))
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    for _ in 0..10 {
+        let counter_rc_clone = Arc::clone(&counter);
+        let handle = std::thread::spawn(move || {
+            let mut val = counter_rc_clone.lock().unwrap();
+            *val += 1;
+        });
+        handles.push(handle)
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("{:?}", counter);
 }
