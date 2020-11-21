60e1de09d94cc9e8ac37226eb40be31ba6a0bb0a
Waleed Samy <waleedsamygd@protonmail.com>
2020-11-21 12:18:57 +0100

    ....

--- src/bin/learn_state_pattern.rs
+++ src/bin/learn_state_pattern.rs
@@ -2,12 +2,13 @@ use hello::blog::Post;
 fn main() {
     let mut post = Post::new();
 
-    post.add_text("text text text");
+    let _ = post.add_text("text text text");
     assert_eq!("", post.content());
 
     post.request_review();
     assert_eq!("", post.content());
 
+    post.approve();
     post.approve();
     assert_eq!("text text text", post.content());
 }
--- src/lib/blog.rs
+++ src/lib/blog.rs
@@ -1,17 +1,24 @@
 pub struct Post {
     state: Option<Box<dyn State>>,
     content: String,
+    reviewer: u8,
 }
 impl Post {
     pub fn new() -> Self {
         Post {
             state: Some(Box::new(Draft {})),
             content: String::new(),
+            reviewer: 0,
         }
     }
 
-    pub fn add_text(&mut self, text: &str) {
-        self.content.push_str(text)
+    pub fn add_text(&mut self, text: &str) -> Result<(), &'static str> {
+        if self.state.as_ref().unwrap().editable() {
+            self.content.push_str(text);
+            Ok(())
+        } else {
+            Err("post is not in Draft state")
+        }
     }
 
     pub fn request_review(&mut self) {
@@ -22,7 +29,8 @@ impl Post {
 
     pub fn approve(&mut self) {
         if let Some(s) = self.state.take() {
-            self.state = Some(s.approve())
+            self.state = Some(s.approve());
+            self.reviewer += 1;
         }
     }
 
@@ -34,8 +42,12 @@ impl Post {
 trait State {
     fn request_review(self: Box<Self>) -> Box<dyn State>;
     fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn reject(self: Box<Self>) -> Box<dyn State>;
     fn content<'a>(&self, _: &'a Post) -> Option<&'a str> {
         None
+    }
+    fn editable(&self) -> bool {
+        false
     }
 }
 #[derive(Debug)]
@@ -47,6 +59,12 @@ impl State for Draft {
     fn approve(self: Box<Self>) -> Box<dyn State> {
         self
     }
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn editable(&self) -> bool {
+        true
+    }
 }
 
 #[derive(Debug)]
@@ -57,6 +75,9 @@ impl State for PendingReview {
     }
     fn approve(self: Box<Self>) -> Box<dyn State> {
         Box::new(Published {})
+    }
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Draft {})
     }
 }
 
@@ -72,6 +93,14 @@ impl State for Published {
     }
 
     fn content<'a>(&self, post: &'a Post) -> Option<&'a str> {
-        Some(post.content.as_str())
+        if post.reviewer >= 2 {
+            Some(post.content.as_str())
+        } else {
+            None
+        }
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Draft {})
     }
 }
