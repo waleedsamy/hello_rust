02d88d7bff515cc02cada6d0ccd92b18f41078c1
Waleed Samy <waleedsamygd@protonmail.com>
2020-11-24 16:45:03 +0100

    ....

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::net::TcpListener;
+use std::{fs, net::TcpListener};
 use std::{io::prelude::*, net::TcpStream};
 
+///
+/// ```no_run
+///  curl 'http://localhost:8080/'
+/// ```
 fn main() -> std::io::Result<()> {
     let addr = "127.0.0.1:8080";
     let listener = TcpListener::bind(addr).unwrap();
@@ -8,21 +12,26 @@ fn main() -> std::io::Result<()> {
     eprintln!("listening {}", addr);
 
     for conn_attempt in listener.incoming() {
-        let mut conn_attempt = conn_attempt.unwrap();
-        handle_conn_attempt(&mut conn_attempt)?;
+        handle_conn_attempt(conn_attempt?)?;
     }
 
     Ok(())
 }
 
-fn handle_conn_attempt(stream: &mut TcpStream) -> std::io::Result<()> {
+fn handle_conn_attempt(mut stream: TcpStream) -> std::io::Result<()> {
     let mut buffer: [u8; 1024] = [0; 1024];
 
     stream.read(&mut buffer)?;
     let request = String::from_utf8_lossy(&buffer);
-    println!("{}\n", request);
+    println!("request:\n{}", request);
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let content = fs::read_to_string("hello.html")?;
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Lenght: {}\r\n\r\n{}",
+        content.len(),
+        content,
+    );
+
     stream.write(response.as_bytes())?;
     stream.flush()?;
     Ok(())
